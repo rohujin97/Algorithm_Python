c757b05bac7885fc7d1153c631ad7276d3fcae73
rohujin97 <rohujin98@naver.com>
2021-12-14 11:51:21 +0900

    16508 전공책 solved

---
+++ baekjoon/16508_전공책_Silver3.py
@@ -0,0 +1,40 @@
+import sys
+from itertools import combinations
+input = sys.stdin.readline
+
+def find(want, word, coin):
+    cnt = 0
+    for w in word:
+        if w in want:
+            cnt += 1
+            want = want.replace(w, ' ', 1)
+        if cnt == len(T):
+            return coin
+    return 987654321
+
+T = input().rstrip()
+N = int(input())
+coins = [0]*N
+words = [0]*N
+
+for i in range(N):
+    coin, word = map(str, input().split())
+    coins[i] = int(coin)
+    words[i] = word.rstrip()
+
+result = []
+num = [i for i in range(N)]
+for i in range(1, N+1):
+    for comb in combinations(num, i):
+        new_word = ''
+        sum_coin = 0
+        for c in comb:
+            new_word += words[c]
+            sum_coin += coins[c]
+        result.append(find(T, new_word, sum_coin))
+
+minC = min(result)
+if minC == 987654321:
+    print(-1)
+else:
+    print(minC)
