dc839d2767a5b248b7ab1073ae196993716b3fa7
rohujin97 <rohujin98@naver.com>
2021-12-16 00:45:05 +0900

    diff code

--- baekjoon/1913_달팽이_Silver5.py
+++ baekjoon/1913_달팽이_Silver5.py
@@ -11,23 +11,17 @@ snail = [[0]*N for _ in range(N)]
 start = N**2
 r, c = 0, 0
 d = 0
-tr, tc = 0, 0
 
 while start > 0:
     if start == T:
         tr, tc = r+1, c+1
     snail[r][c] = start
     start -= 1
-    nr = r + dr[d]
-    nc = c + dc[d]
 
-    if nr < 0 or nr >= N or nc < 0 or nc >= N or snail[nr][nc] != 0:
+    if r + dr[d] < 0 or r + dr[d] >= N or c + dc[d] < 0 or c + dc[d] >= N or snail[r+dr[d]][c+dc[d]] != 0:
         d = (d + 1) % 4
-        r += dr[d]
-        c += dc[d]
-    else:
-        r = nr
-        c = nc
+    r += dr[d]
+    c += dc[d]
 
 for n in snail:
     print(' '.join(map(str, n)))
