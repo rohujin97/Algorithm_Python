c0057d5244767f650057471c1072220e1e6b365b
rohujin97 <rohujin98@naver.com>
2021-12-18 22:59:51 +0900

    2578 new solving

--- baekjoon/2578_빙고_Silver5.py
+++ baekjoon/2578_빙고_Silver5.py
@@ -1,48 +1,46 @@
-def find():
-    global total, slash, bslash, row, col
+import sys
+input = sys.stdin.readline
+
+def check(chul):
+    global count
+    dialog, b_dialog = 0, 0
+
     for i in range(5):
-        if row[i] == 5:
-            total += 1
-            row[i] = 0
-        if col[i] == 5:
-            total += 1
-            col[i] = 0
-    if bslash == 5:
-        total += 1
-        bslash = 0
-    if slash == 5:
-        total += 1
-        slash = 0
+        if str(chul[i]).count('[0, 0, 0, 0, 0]') and not visited_row[i]:
+            count += 1
+            visited_row[i] = True
 
+        col = list(map(lambda x:x[i], chul))
+        if str(col).count('[0, 0, 0, 0, 0]') and not visited_col[i]:
+            count += 1
+            visited_col[i] = True
+        dialog += chul[i][i]
+        b_dialog += chul[i][4-i]
+
+    if dialog == 0 and not visited_dialog[0]:
+        count += 1
+        visited_dialog[0] = True
+
+    if b_dialog == 0 and not visited_dialog[1]:
+        count += 1
+        visited_dialog[1] = True
+
+    return count
+
+count, order = 0, 0
+visited_row, visited_col, visited_dialog = [False]*5, [False]*5, [False]*2
 chul = [list(map(int, input().split())) for _ in range(5)]
-mc = [list(map(int, input().split())) for _ in range(5)]
-location = [[]]*26
-col = [0]*5
-row = [0]*5
-slash = 0
-bslash = 0
+mc = []
+for _ in range(5):
+    mc += list(map(int, input().split()))
 
-for i in range(5):
-    for j in range(5):
-        location[chul[i][j]] = [i, j]
-total = 0
-cnt = 0
-result = False
-for i in range(5):
-    for j in range(5):
-        cnt += 1
-        r = location[mc[i][j]][0]
-        c = location[mc[i][j]][1]
-        row[r] += 1
-        col[c] += 1
-        if r + c == 4:
-            bslash += 1
-        if r == c:
-            slash += 1
-        find()
-        if total >= 3:
-            print(cnt)
-            result = True
-            break
-    if result:
-        break
+for num in range(25):
+    for i in range(5):
+        for j in range(5):
+            if mc[num] == chul[i][j]:
+                chul[i][j] = 0
+                check(chul)
+                order += 1
+                if count >= 3:
+                    print(order)
+                    exit()
